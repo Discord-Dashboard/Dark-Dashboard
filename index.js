aa287b58be0583792b6ebfc8bb5282b6b079c374
PlainDevelopment <66025383+PlainDevelopment@users.noreply.github.com>
2021-12-28 10:58:36 +0000

    v1.5.1

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ module.exports = (themeConfig = {}) => {
                 }
                 
                 
-                res.render('debug', {bot: config.bot, req: req, config: config, rawUptime: process.uptime(), onlineFiles, localFiles, nodeVersion: process.version, themeConfig: themeConfig, discordVersion: require('discord.js').version, dbdVersion: require('./node_modules/discord-dashboard/package.json').version, themeVersion: require('./package.json').version});
+                res.render('debug', {bot: config.bot, req: req, config: config, rawUptime: process.uptime(), onlineFiles, localFiles, nodeVersion: process.version, themeConfig: themeConfig, discordVersion: require('discord.js').version, dbdVersion: require('../discord-dashboard/package.json').version, themeVersion: require('./package.json').version});
             });
         }
     };
